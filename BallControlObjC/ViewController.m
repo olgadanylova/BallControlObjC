cb4843a666ca0d8108ba6917108ceb8a9fd6f160
Olga Danylova <olga@themidnightcoders.com>
2018-04-15 21:19:43 +0300

    Update ViewController.m

--- BallControlObjC/ViewController.m
+++ BallControlObjC/ViewController.m
@@ -12,6 +12,10 @@
     NSDictionary *locationDictionary;
     SharedObject *sharedObject;
     BOOL imageMoveEnabled;
+    
+    CGFloat minX, maxX, minY, maxY;
+    float koefX, koefY;
+    BOOL dragging;
 }
 @end
 
@@ -19,6 +23,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    dragging = NO;
+    
+    minX = 0;
+    maxX = self.view.frame.size.width - self.imageView.frame.size.width;
+    minY = 0;
+    maxY = self.view.frame.size.height - self.imageView.frame.size.height;
+    
+    [self calculateKoef];
     
     self.textView.hidden = NO;
     self.imageView.hidden = YES;
@@ -29,7 +42,30 @@
     [backendless initApp:APP_ID APIKey:API_KEY];
     
     sharedObject = [backendless.sharedObject connect:SHARED_OBJECT_NAME];
+    [self addOnChangesListener];
     [self addRTListeners];
+}
+
+-(void)calculateKoef {
+    koefX = self.imageView.frame.origin.x / maxX;
+    koefY = self.imageView.frame.origin.y / maxY;
+    
+    if (koefX < 0) {
+        koefX = 0;
+    }
+    if (koefX > 1) {
+        koefX = 1;
+    }
+    if (koefY < 0) {
+        koefY = 0;
+    }
+    if (koefY > 1) {
+        koefY = 1;
+    }
+    //
+    //    NSLog(@"koefX = %f", koefX);
+    //    NSLog(@"koefY = %f", koefY);
+    //    NSLog(@"__________");
 }
 
 -(void)addRTListeners {
@@ -48,12 +84,14 @@
     
     [backendless.rt addConnectErrorEventListener:^(NSString *connectError) {
         sharedObject = [backendless.sharedObject connect:SHARED_OBJECT_NAME];
+        [self addOnChangesListener];
         NSLog(@"Status: connection failed (%@)", connectError);
         self.connectButton.enabled = NO;
     }];
     
     [backendless.rt addDisonnectEventListener:^(NSString *disconnectReason) {
         sharedObject = [backendless.sharedObject connect:SHARED_OBJECT_NAME];
+        [self addOnChangesListener];
         self.navigationItem.title = [NSString stringWithFormat:@"Status: disconnected (%@)", disconnectReason];
         self.connectButton.enabled = NO;
     }];
@@ -63,15 +101,47 @@
     }];
 }
 
--(void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
-    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
-    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
-        locationDictionary = @{@"x" : [NSNumber numberWithFloat:location.x], @"y" : [NSNumber numberWithFloat:location.y]};
-        [self animate:locationDictionary];
-    } completion:^(id<UIViewControllerTransitionCoordinatorContext> context) {
-    }];
+-(void)addOnChangesListener {
     
+    __weak __block ViewController *weakSelf = self;
+    [sharedObject addChangesListener:^(SharedObjectChanges *changes) {
+        // move imageView
+        float xK = [[changes.data valueForKey:@"xKoef"] floatValue];
+        float yK = [[changes.data valueForKey:@"yKoef"] floatValue];
+        
+        //        CGFloat locationX = [[locationDictionary valueForKey:@"x"] floatValue];
+        //        CGFloat locationY = [[locationDictionary valueForKey:@"y"] floatValue];
+        
+        CGFloat originX = xK * maxX;//locationX - self.imageView.frame.size.width / 2;
+        CGFloat originY = yK * maxY;//locationY - self.imageView.frame.size.height / 2;
+        
+        NSLog(@"oX = %f, oY = %f", originX, originY);
+        if (!dragging) {
+            NSLog(@"WILL move");
+            
+            [UIView animateWithDuration:0.3 animations:^{
+                weakSelf.imageView.frame = CGRectMake(originX, originY, self.imageView.frame.size.width, self.imageView.frame.size.height);
+            }];
+        }
+        
+        
+        //        [UIView animateWithDuration:0.3 animations:^{
+        //            weakSelf.imageView.frame = CGRectMake(originX, originY, self.imageView.frame.size.width, self.imageView.frame.size.height);
+        //        }];
+        
+    } error:^(Fault *fault) {
+        
+    }];
 }
+
+//-(void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+//    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+//    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+//        locationDictionary = @{@"x" : [NSNumber numberWithFloat:location.x], @"y" : [NSNumber numberWithFloat:location.y]};
+//        [self animate:locationDictionary];
+//    } completion:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+//    }];
+//}
 
 - (IBAction)animate:(id)sender {
     locationDictionary = sender;
@@ -102,13 +172,32 @@
     }];
 }
 
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    dragging = YES;
+}
+
+-(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    dragging = NO;
+}
+
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self calculateKoef];
     if (imageMoveEnabled) {
         UITouch *touch = [touches anyObject];
         location = [touch locationInView:self.view];
         
+        self.imageView.center = location;
+        locationDictionary = @{@"xKoef" : [NSNumber numberWithFloat:koefX], @"yKoef" : [NSNumber numberWithFloat:koefY]};
+        [sharedObject set:@"locKoefs" data:locationDictionary response:^(id result) {
+            
+        } error:^(Fault *fault) {
+            
+        }];
+        
+        
         CGFloat statusBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height;
         CGFloat navBarHeight = self.navigationController.navigationBar.frame.size.height;
+        
         
         if (location.x - self.imageView.frame.size.width / 2 <= CGRectGetMinX(self.view.frame)) {
             location.x = self.imageView.frame.size.width / 2;
@@ -123,11 +212,11 @@
             location.y = CGRectGetMaxY(self.view.frame) - self.imageView.frame.size.height / 2;
         }
         
-        self.imageView.center = location;
-        locationDictionary = @{@"x" : [NSNumber numberWithFloat:location.x], @"y" : [NSNumber numberWithFloat:location.y]};
-        [sharedObject invokeOn:@"animate" targets:@[[NSNull null]] args:@[locationDictionary] response:^(id res) {
-        } error:^(Fault *fault) {
-        }];
+        //        self.imageView.center = location;
+        //        locationDictionary = @{@"x" : [NSNumber numberWithFloat:location.x], @"y" : [NSNumber numberWithFloat:location.y]};
+        //        [sharedObject invokeOn:@"animate" targets:@[[NSNull null]] args:@[locationDictionary] response:^(id res) {
+        //        } error:^(Fault *fault) {
+        //        }];
     }
 }
 
